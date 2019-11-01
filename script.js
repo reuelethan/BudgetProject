f04b7e21e9a6c562ea3c056122d44358a3ba0333
Dewanlb <dewanok@gmail.com>
2019-11-01 13:41:29 -0400

    Working Functions

--- script.js
+++ script.js
@@ -2,12 +2,57 @@ document.querySelector(".weeklyBudget").innerHTML = `
 <div class="weeklybudget"> Number Goes Here
 </div> `;
 
-
 //test 2//
-let earned = (document.querySelector("#earnings"));
-document.querySelector("#earned").addEventListener("submit", function budgetValue(e) {
-    console.log(earned.value);
-    document.querySelector(".weeklyBudget").innerHTML = (`$${earned.value}`);
-    e.preventDefault();
+let earned;
+let price;
+let items;
+class Information {
+  constructor(item, price) {
+    this.item = item;
+    this.price = price;
+  }
+}
 
-})
+class Expenses {
+  constructor() {
+    this.expenses = [];
+  }
+  add(item, price) {
+    this.expenses = [...this.expenses, new Information(item, price)];
+  }
+}
+
+document
+  //form
+  .querySelector("#earned")
+  .addEventListener("submit", function budgetValue(e) {
+    earned = document.querySelector("#earnings");
+    document.querySelector(".weeklyBudget").innerHTML = `$${earned.value}`;
+    e.preventDefault();
+    console.log(earned.value);
+  });
+
+//logs prices of items
+items = document.querySelector("#Items");
+price = document.querySelector("#itemsPrice");
+document
+  .querySelector("#data")
+  .addEventListener("submit", function expenseData(event) {
+    event.preventDefault();
+    document.querySelector(
+      ".expenseLog"
+    ).innerHTML = `<div>${price.value}</div>`;
+    console.log(price.value);
+
+    function display(expenseListReference) {
+      expenseListReference.expenses.forEach(expense => {
+        console.log(expense);
+      });
+    }
+
+    const expenseList = new Expenses();
+    expenseList.add("Mitch", "mitch@grandcircus.co");
+    expenseList.add("Tyler", "tyler@email.com");
+    expenseList.add("John", "john@email.com");
+    display(expenseList);
+  });
